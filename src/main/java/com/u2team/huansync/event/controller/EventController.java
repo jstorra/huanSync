f9a0b9e89c77de8560e010cb07398280d42930b8
KazeVentum <132288586+KazeVentum@users.noreply.github.com.>
2024-01-20 21:18:51 -0500

    feat: :sparkles: creation event crud
    mvc, dao pattern, builder event

--- src/main/java/com/u2team/huansync/event/controller/EventController.java
+++ src/main/java/com/u2team/huansync/event/controller/EventController.java
@@ -1,9 +1,39 @@
 package com.u2team.huansync.event.controller;
 
+import com.u2team.huansync.event.model.DAO.EventDAO;
+import com.u2team.huansync.event.model.classes.Event;
+import java.util.List;
+
 /**
  *
  * @author Usuario
  */
 public class EventController {
-    
+
+    private EventDAO eventDao;
+
+    public EventController() {
+        this.eventDao = new EventDAO();
+    }
+
+    public Event getById(long eventId) {
+        return eventDao.getById(eventId);
+    }
+
+    public List<Event> getAllEvents() {
+        return eventDao.getAll();
+    }
+
+    public void insertEvent(Event event) {
+        eventDao.save(event);
+    }
+
+    public void deleteEvent(long eventId) {
+        eventDao.delete(eventId);
+    }
+
+    public void updateEvent(Event event) {
+        eventDao.update(event);
+    }
+
 }

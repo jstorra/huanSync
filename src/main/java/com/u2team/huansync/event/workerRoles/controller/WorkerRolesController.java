e91e9a53f90a29a6b79b2a685b0806b3927fa496
JCristinaJSalas <89360648+JCristinaJSalas@users.noreply.github.com>
2024-01-21 12:17:29 -0500

    feat: :sparkles: Create WorkerRole controller

---
+++ src/main/java/com/u2team/huansync/event/workerRoles/controller/WorkerRolesController.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.u2team.huansync.event.workerRoles.controller;
+
+/**
+ *
+ * @author criis
+ */
+public class WorkerRolesController {
+    
+}

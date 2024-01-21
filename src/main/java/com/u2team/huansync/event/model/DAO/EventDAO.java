b7c9251bf4f43a69dcdec1cea2a1bb9f1d500e0d
KazeVentum <132288586+KazeVentum@users.noreply.github.com.>
2024-01-20 21:18:51 -0500

    feat: :sparkles: creation event crud
    mvc, dao pattern, builder event

--- src/main/java/com/u2team/huansync/event/model/DAO/EventDAO.java
+++ src/main/java/com/u2team/huansync/event/model/DAO/EventDAO.java
@@ -3,31 +3,30 @@ package com.u2team.huansync.event.model.DAO;
 import com.u2team.huansync.event.model.classes.builders.EventBuilder;
 import com.u2team.huansync.event.model.classes.builders.EventConcreteBuilder;
 import com.u2team.huansync.event.model.classes.Event;
-import com.u2team.huansync.persistencia.*;
+import com.u2team.huansync.event.model.util.Validations;
+import com.u2team.huansync.persistence.*;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.*;
-import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 /**
  *
- * @author Usuario
+ * @author Christian Pardo
  */
 public class EventDAO implements IDao<Event> {
 
     @Override
     public Event getById(long id) {
 
-        Operaciones.setConnection(ConexionBD.MySQLConnection());
+        Operations.setConnection(BDConnection.MySQLConnection());
         String stm = "SELECT * FROM tbl_events where eventId = ?;";
 
-        try (PreparedStatement ps = Operaciones.getConnection().prepareStatement(stm)) {
+        try (PreparedStatement ps = Operations.getConnection().prepareStatement(stm)) {
             ps.setLong(1, id);
-            ResultSet rs = Operaciones.consultar_BD(ps);
+            ResultSet rs = Operations.query_db(ps);
             if (rs.next()) {
                 EventBuilder eventBuilder = new EventConcreteBuilder();
 
@@ -47,6 +46,8 @@ public class EventDAO implements IDao<Event> {
                         .build();
 
                 return sqlEvent;
+            } else {
+                System.out.println("ERROR: The id has not been found");
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -58,11 +59,11 @@ public class EventDAO implements IDao<Event> {
     public List<Event> getAll() {
         List<Event> eventList = new ArrayList<>();
 
-        Operaciones.setConnection(ConexionBD.MySQLConnection());
+        Operations.setConnection(BDConnection.MySQLConnection());
         String stm = "SELECT * FROM tbl_events";
 
-        try (PreparedStatement ps = Operaciones.getConnection().prepareStatement(stm)) {
-            ResultSet rs = ps.executeQuery();
+        try (PreparedStatement ps = Operations.getConnection().prepareStatement(stm)) {
+            ResultSet rs = Operations.query_db(ps);
 
             while (rs.next()) {
                 EventBuilder eventBuilder = new EventConcreteBuilder();
@@ -92,32 +93,22 @@ public class EventDAO implements IDao<Event> {
 
     @Override
     public void save(Event event) {
-        LocalDate curDate = LocalDate.now();
-        long difDate = ChronoUnit.DAYS.between(curDate, event.getDateEvent());
-        if (difDate < 1 || difDate > 7) {
+
+        boolean validDate = Validations.dateBetween(LocalDate.now(), event.getDateEvent(), 1, 7);
+
+        if (validDate) {
             System.out.println("Invalid date");
             return;
         }
 
-        Operaciones.setConnection(ConexionBD.MySQLConnection());
-        String stm = "select count(eventId) as repeatedNames from tbl_events WHERE nameEvent = ?;";
-
-        try (PreparedStatement ps = Operaciones.getConnection().prepareStatement(stm)) {
-            ps.setString(1, event.getNameEvent());
-            ResultSet rs = Operaciones.consultar_BD(ps);
-            if (rs.next()) {
-                int repeatedNames = rs.getInt("repeatedNames");
-                if (repeatedNames > 0) {
-                    System.out.println("Already name exists");
-                    return;
-                }
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
+        int repeated = Validations.counterRepeated("tbl_events", "nameEvent", "asdasdasd");
+        if (repeated != 0) {
+            System.out.println("nameEvent repeated");
+            return;
         }
 
         String stmInsert = "INSERT INTO tbl_events(nameEvent, countryEvent, cityEvent, addressEvent, peopleCapacity, storeCapacity, restaurantCapacity, dateEvent, timeEvent, organizerId, ageClassification, status)  VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
-        try (PreparedStatement ps = Operaciones.getConnection().prepareStatement(stmInsert)) {
+        try (PreparedStatement ps = Operations.getConnection().prepareStatement(stmInsert)) {
             ps.setString(1, event.getNameEvent());
             ps.setString(2, event.getCountry());
             ps.setString(3, event.getCity());
@@ -131,7 +122,7 @@ public class EventDAO implements IDao<Event> {
             ps.setString(11, event.getAgeClassification().name());
             ps.setString(12, event.getStatusEnum().name());
 
-            int rows = Operaciones.insertar_actualizar_borrar_BD(ps);
+            int rows = Operations.insert_update_delete_db(ps);
             if (rows <= 0) {
                 System.out.println("Cannot insert event");
             } else {
@@ -146,53 +137,76 @@ public class EventDAO implements IDao<Event> {
     public void update(Event event) {
         Event sqlEvent = getById(event.getEventId());
 
-        if (sqlEvent != null) {
-            sqlEvent.setNameEvent(event.getNameEvent());
-            sqlEvent.setCountry(event.getCountry());
-            sqlEvent.setCity(event.getCity());
-            sqlEvent.setAddress(event.getAddress());
-            sqlEvent.setPeopleCapacity(event.getPeopleCapacity());
-            sqlEvent.setStoreCapacity(event.getStoreCapacity());
-            sqlEvent.setRestaurantCapacity(event.getRestaurantCapacity());
-            sqlEvent.setDateEvent(event.getDateEvent());
-            sqlEvent.setTimeEvent(event.getTimeEvent());
-            sqlEvent.setOrganizerId(event.getOrganizerId());
-            sqlEvent.setAgeClassification(event.getAgeClassification());
-            sqlEvent.setStatusEnum(event.getStatusEnum());
-            
-                @Override
-    public void update(Event event) {
-        Event sqlEvent = getById(event.getEventId());
+        boolean validDate = Validations.dateBetween(LocalDate.now(), event.getDateEvent(), 1, 7);
 
-        if (sqlEvent != null) {
-            sqlEvent.setNameEvent(event.getNameEvent());
-            sqlEvent.setCountry(event.getCountry());
-            sqlEvent.setCity(event.getCity());
-            sqlEvent.setAddress(event.getAddress());
-            sqlEvent.setPeopleCapacity(event.getPeopleCapacity());
-            sqlEvent.setStoreCapacity(event.getStoreCapacity());
-            sqlEvent.setRestaurantCapacity(event.getRestaurantCapacity());
-            sqlEvent.setDateEvent(event.getDateEvent());
-            sqlEvent.setTimeEvent(event.getTimeEvent());
-            sqlEvent.setOrganizerId(event.getOrganizerId());
-            sqlEvent.setAgeClassification(event.getAgeClassification());
-            sqlEvent.setStatusEnum(event.getStatusEnum());
-            
-            
-            
-            
-            
-            
-        } else {
-            System.out.println("not found results event");
+        if (!validDate) {
+            System.out.println("Invalid date");
+            return;
         }
 
-    }
+        int repeated = Validations.counterRepeated("tbl_events", "nameEvent", "asdasdasd");
+        if (repeated != 0) {
+            System.out.println("nameEvent repeated");
+            return;
+        }
 
-            
-            
-            
-            
+        if (sqlEvent != null) {
+            sqlEvent.setNameEvent(event.getNameEvent());
+            sqlEvent.setCountry(event.getCountry());
+            sqlEvent.setCity(event.getCity());
+            sqlEvent.setAddress(event.getAddress());
+            sqlEvent.setPeopleCapacity(event.getPeopleCapacity());
+            sqlEvent.setStoreCapacity(event.getStoreCapacity());
+            sqlEvent.setRestaurantCapacity(event.getRestaurantCapacity());
+            sqlEvent.setDateEvent(event.getDateEvent());
+            sqlEvent.setTimeEvent(event.getTimeEvent());
+            sqlEvent.setOrganizerId(event.getOrganizerId());
+            sqlEvent.setAgeClassification(event.getAgeClassification());
+            sqlEvent.setStatusEnum(event.getStatusEnum());
+
+            String stmInsert = """
+            UPDATE tbl_events
+            SET nameEvent = ?,
+                countryEvent = ?,
+                cityEvent = ?,
+                addressEvent = ?,
+                peopleCapacity = ?,
+                storeCapacity = ?,
+                restaurantCapacity = ?,
+                dateEvent = ?,
+                timeEvent = ?,   
+                organizerId = ?,
+                ageClassification = ?,
+                status = ?
+            WHERE eventId = ?;
+                               """;
+            try (PreparedStatement ps = Operations.getConnection().prepareStatement(stmInsert)) {
+                ps.setString(1, event.getNameEvent());
+                ps.setString(2, event.getCountry());
+                ps.setString(3, event.getCity());
+                ps.setString(4, event.getAddress());
+                ps.setInt(5, event.getPeopleCapacity());
+                ps.setInt(6, event.getStoreCapacity());
+                ps.setInt(7, event.getRestaurantCapacity());
+                ps.setString(8, event.getDateEvent().toString());
+                ps.setString(9, event.getTimeEvent().toString());
+                ps.setLong(10, event.getOrganizerId());
+                ps.setString(11, event.getAgeClassification().name());
+                ps.setString(12, event.getStatusEnum().name());
+                ps.setLong(13, event.getEventId());
+
+                System.out.println(ps.toString());
+
+                int rows = Operations.insert_update_delete_db(ps);
+                if (rows <= 0) {
+                    System.out.println("Cannot update event");
+                } else {
+                    System.out.println("Successful update event");
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
         } else {
             System.out.println("not found results event");
         }
@@ -202,12 +216,12 @@ public class EventDAO implements IDao<Event> {
     @Override
     public void delete(long eventId) {
 
-        Operaciones.setConnection(ConexionBD.MySQLConnection());
+        Operations.setConnection(BDConnection.MySQLConnection());
         String stm = "DELETE FROM tbl_events WHERE eventId = ?;";
 
-        try (PreparedStatement ps = Operaciones.getConnection().prepareStatement(stm)) {
+        try (PreparedStatement ps = Operations.getConnection().prepareStatement(stm)) {
             ps.setLong(1, eventId);
-            int rows = Operaciones.insertar_actualizar_borrar_BD(ps);
+            int rows = Operations.insert_update_delete_db(ps);
             if (rows > 0) {
                 System.out.println("succesful delete event");
                 return;
--- src/main/java/com/u2team/huansync/event/model/DAO/IDao.java
+++ src/main/java/com/u2team/huansync/event/model/DAO/IDao.java
@@ -1,7 +1,6 @@
 package com.u2team.huansync.event.model.DAO;
 
 import java.util.List;
-import java.util.Optional;
 
 /**
  *

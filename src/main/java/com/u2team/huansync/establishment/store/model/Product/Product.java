0976dfc2c4d99ec0f9b8dde31129a75791bfa701
BarbatosLopux <sneideresteban@gmail.com>
2024-01-21 20:12:24 -0500

    feat: :construction: Construction Code in progress whit classes
    In progress construction structure code backend

---
+++ src/main/java/com/u2team/huansync/establishment/store/model/Product/Product.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.u2team.huansync.establishment.store.model;
+
+/**
+ *
+ * @author sneid
+ */
+public class Product {
+    
+}

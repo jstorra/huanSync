dbdf59a6898176c871949ed0ba5b8e236b59e3e3
Jaider Mendoza <jaiderstyvenmendoza@gmail.com>
2024-01-22 17:28:44 -0500

    fix: :bug: fix unnecessary package target since 0cd987d

--- src/main/java/com/u2team/huansync/view/staffView/StaffView.java
+++ src/main/java/com/u2team/huansync/view/staffView/StaffView.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package com.u2team.huansync.view.StaffView;
+package com.u2team.huansync.view.staffView;
 
 /**
  *

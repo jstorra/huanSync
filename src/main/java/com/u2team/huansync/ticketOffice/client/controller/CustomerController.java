4fc62f6df9b58a7bf4b870147fbaf2828766ef6b
DavidSantos004 <ds0299646@gmail.com>
2024-01-23 07:29:45 -0500

    feat: :sparkles: code documentation
    all client code was documented

--- src/main/java/com/u2team/huansync/ticketOffice/client/controller/CustomerController.java
+++ src/main/java/com/u2team/huansync/ticketOffice/client/controller/CustomerController.java
@@ -1,3 +1,7 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package com.u2team.huansync.ticketOffice.client.controller;
 
 import com.u2team.huansync.ticketOffice.client.model.DAO.CustomerDAO;
@@ -5,32 +9,93 @@ import com.u2team.huansync.ticketOffice.client.model.classes.Customer;
 
 import java.util.List;
 
+/**
+ * Controller class for managing interactions between the UI and the Customer data model.
+ * This class provides methods to perform CRUD operations on customer entities.
+ *
+ * Usage example:
+ * <pre>{@code
+ * CustomerController customerController = new CustomerController();
+ *
+ * // Retrieve a customer by ID
+ * Customer customer = customerController.getByIdCustomer(1);
+ *
+ * // Retrieve all customers
+ * List<Customer> allCustomers = customerController.getAllCustomer();
+ *
+ * // Save a new customer
+ * Customer newCustomer = new Customer();
+ * // Set customer properties...
+ * customerController.insertCustomer(newCustomer);
+ *
+ * // Update an existing customer
+ * Customer existingCustomer = customerController.getByIdCustomer(1);
+ * // Modify customer properties...
+ * customerController.updateCustomer(existingCustomer);
+ *
+ * // Delete a customer by ID
+ * customerController.deleteCustomer(1);
+ * }</pre>
+ *
+ * @author [Your Name]
+ */
 public class CustomerController {
 
+    /**
+     * The CustomerDAO instance for interacting with the data model.
+     */
     private CustomerDAO customerDAO;
 
-    public CustomerController()   {
+    /**
+     * Constructs a new CustomerController instance with a default CustomerDAO.
+     */
+    public CustomerController() {
         this.customerDAO = new CustomerDAO();
     }
 
-    public Customer getByIdCustomer(long customerId){
+    /**
+     * Retrieves a customer by its unique identifier.
+     *
+     * @param customerId The unique identifier of the customer.
+     * @return The customer with the specified identifier, or null if not found.
+     */
+    public Customer getByIdCustomer(long customerId) {
         return customerDAO.getById(customerId);
     }
 
-    public List<Customer> getAllCustomer(){
+    /**
+     * Retrieves a list of all customers.
+     *
+     * @return A list containing all customers.
+     */
+    public List<Customer> getAllCustomer() {
         return customerDAO.getAll();
     }
-    public void insertCustomer (Customer customer){
+
+    /**
+     * Inserts a new customer into the data model.
+     *
+     * @param customer The customer to be inserted.
+     */
+    public void insertCustomer(Customer customer) {
         customerDAO.save(customer);
     }
-    
-    public void deleteCustomer(long customerId){
+
+    /**
+     * Deletes a customer by its unique identifier from the data model.
+     *
+     * @param customerId The unique identifier of the customer to be deleted.
+     */
+    public void deleteCustomer(long customerId) {
         customerDAO.delete(customerId);
     }
-    
-    public void updateCustomer(Customer customer){
+
+    /**
+     * Updates an existing customer in the data model.
+     *
+     * @param customer The customer to be updated.
+     */
+    public void updateCustomer(Customer customer) {
         customerDAO.update(customer);
     }
-
-
 }

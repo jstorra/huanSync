b974c577529b75feac32dcd178a5164d9c287cf3
AitsuYuyu <136203413+AitsuYuyu@users.noreply.github.com>
2024-01-23 16:26:20 -0500

    feat: :sparkles: Add change interface

--- src/main/java/com/u2team/huansync/view/InitialView.java
+++ src/main/java/com/u2team/huansync/view/InitialView.java
@@ -27,24 +27,25 @@ public class InitialView extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        buttonStart = new javax.swing.JButton();
+        btnStart = new javax.swing.JButton();
         welcomeTittle = new javax.swing.JLabel();
-        jLabel1 = new javax.swing.JLabel();
+        HuanSyncTittle = new javax.swing.JLabel();
+        Naruto = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jPanel1.setBackground(new java.awt.Color(204, 204, 204));
+        jPanel1.setBackground(new java.awt.Color(255, 148, 50));
         jPanel1.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
         jPanel1.setForeground(new java.awt.Color(0, 0, 0));
 
-        buttonStart.setBackground(new java.awt.Color(153, 153, 153));
-        buttonStart.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        buttonStart.setForeground(new java.awt.Color(51, 51, 51));
-        buttonStart.setText("START");
-        buttonStart.setBorder(javax.swing.BorderFactory.createCompoundBorder());
-        buttonStart.addActionListener(new java.awt.event.ActionListener() {
+        btnStart.setBackground(new java.awt.Color(231, 0, 33));
+        btnStart.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        btnStart.setForeground(new java.awt.Color(255, 255, 255));
+        btnStart.setText("START");
+        btnStart.setBorder(javax.swing.BorderFactory.createCompoundBorder());
+        btnStart.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                buttonStartActionPerformed(evt);
+                btnStartActionPerformed(evt);
             }
         });
 
@@ -52,9 +53,12 @@ public class InitialView extends javax.swing.JFrame {
         welcomeTittle.setForeground(new java.awt.Color(0, 0, 0));
         welcomeTittle.setText("WELCOME TO");
 
-        jLabel1.setFont(new java.awt.Font("Yu Gothic UI Semilight", 1, 36)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(0, 51, 51));
-        jLabel1.setText("HuanSync");
+        HuanSyncTittle.setBackground(new java.awt.Color(0, 0, 0));
+        HuanSyncTittle.setFont(new java.awt.Font("Segoe UI Symbol", 1, 48)); // NOI18N
+        HuanSyncTittle.setForeground(new java.awt.Color(0, 0, 0));
+        HuanSyncTittle.setText("HuanSync");
+
+        Naruto.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/u2team/huansync/view/icons/naruto.png"))); // NOI18N
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -63,26 +67,31 @@ public class InitialView extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(240, 240, 240)
-                        .addComponent(jLabel1))
+                        .addGap(442, 442, 442)
+                        .addComponent(Naruto))
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(279, 279, 279)
-                        .addComponent(buttonStart, javax.swing.GroupLayout.PREFERRED_SIZE, 79, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(415, 415, 415)
+                        .addComponent(welcomeTittle))
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(264, 264, 264)
-                        .addComponent(welcomeTittle)))
-                .addContainerGap(248, Short.MAX_VALUE))
+                        .addGap(370, 370, 370)
+                        .addComponent(HuanSyncTittle))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(441, 441, 441)
+                        .addComponent(btnStart, javax.swing.GroupLayout.PREFERRED_SIZE, 79, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(380, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addGap(36, 36, 36)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(42, 42, 42)
                 .addComponent(welcomeTittle)
-                .addGap(48, 48, 48)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(61, 61, 61)
-                .addComponent(buttonStart)
-                .addContainerGap(74, Short.MAX_VALUE))
+                .addGap(32, 32, 32)
+                .addComponent(Naruto)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(HuanSyncTittle)
+                .addGap(37, 37, 37)
+                .addComponent(btnStart, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(48, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -92,22 +101,25 @@ public class InitialView extends javax.swing.JFrame {
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
+                .addGap(22, 22, 22))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void buttonStartActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonStartActionPerformed
+    private void btnStartActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnStartActionPerformed
         MenuView menu = new MenuView();
         menu.setVisible(true);
+        menu.setLocationRelativeTo(null);
         this.dispose();
         
-    }//GEN-LAST:event_buttonStartActionPerformed
+    }//GEN-LAST:event_btnStartActionPerformed
 
 
     /**
@@ -141,18 +153,21 @@ public class InitialView extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new InitialView().setVisible(true);
+               
+                
             }
         });
         InitialView initial = new InitialView();
         initial.setVisible(true);
+        initial.setLocationRelativeTo(null);
         
     }
     
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton buttonStart;
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel HuanSyncTittle;
+    private javax.swing.JLabel Naruto;
+    private javax.swing.JButton btnStart;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JLabel welcomeTittle;
     // End of variables declaration//GEN-END:variables

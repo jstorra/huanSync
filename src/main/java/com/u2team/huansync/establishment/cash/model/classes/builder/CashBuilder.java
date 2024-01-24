029b04c8dd30bf37f5dfa577ceb03023e2106c73
Trullez-44 <136148374+Trullez-44@users.noreply.github.com>
2024-01-24 13:43:33 -0500

    docs: :memo: Using JavaDocs to documentate the classes of "Cash"

--- src/main/java/com/u2team/huansync/establishment/cash/model/classes/builder/CashBuilder.java
+++ src/main/java/com/u2team/huansync/establishment/cash/model/classes/builder/CashBuilder.java
@@ -1,21 +1,47 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package com.u2team.huansync.establishment.cash.model.classes.builder;
 
 import com.u2team.huansync.establishment.cash.model.classes.Cash;
+
 import java.util.ArrayList;
 
 /**
- *
- * @author DeuryZ
+ * The CashBuilder interface defines a contract for building Cash objects.
+ * Classes implementing this interface should provide methods for setting various
+ * properties of a Cash object and ultimately building the Cash instance.
  */
-
 public interface CashBuilder {
+
+    /**
+     * Sets the state of the Cash object.
+     *
+     * @param state The state of the Cash object (true for active, false for inactive).
+     * @return The CashBuilder instance for method chaining.
+     */
     CashBuilder state(boolean state);
+
+    /**
+     * Sets the opening amount of the Cash object.
+     *
+     * @param openingAmount The opening amount of the Cash object.
+     * @return The CashBuilder instance for method chaining.
+     */
     CashBuilder openingAmount(double openingAmount);
+
+    /**
+     * Sets the closing amount of the Cash object.
+     *
+     * @param closingAmount The closing amount of the Cash object.
+     * @return The CashBuilder instance for method chaining.
+     */
     CashBuilder closingAmount(double closingAmount);
-//    CashBuilder  order (ArrayList<Order> order);
+
+    // Uncomment the following line if Order class is available
+    // CashBuilder order(ArrayList<Order> order);
+
+    /**
+     * Builds and returns the Cash object based on the provided properties.
+     *
+     * @return The constructed Cash object.
+     */
     Cash build();
 }

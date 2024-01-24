afe5a94cfad2987e9925de327907d652a386b07d
Kevin-Jimenez-D <122753141+Kevin-Jimenez-D@users.noreply.github.com>
2024-01-24 00:19:10 -0500

    feat: :construction: I am creating the CRUD with Order
    I implement with getAll and save but I have a mistake

---
+++ src/main/java/com/u2team/huansync/establishment/order/model/classes/Order.java
@@ -0,0 +1,101 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.u2team.huansync.establishment.order.model.classes;
+
+/**
+ *
+ * @author Dereck Noa
+ */
+public class Order {
+    
+    private long orderId;
+    private int customerId;
+    private int establishmentId;
+    private int cashRegisterId;
+    private int cashierOperatorId;
+    private OrderStatusEnum orderStatusEnum;
+    
+    //Empty Constructor 
+    public Order() {
+        
+    }
+    
+    //Constructor
+    public Order(long orderId, int customerId, int establishmentId, int cashId, int cashierOperatorId, OrderStatusEnum orderStatusEnum) {
+        this.orderId = orderId;
+        this.customerId = customerId;
+        this.establishmentId = establishmentId;
+        this.cashRegisterId = cashId;
+        this.cashierOperatorId = cashierOperatorId;
+        this.orderStatusEnum = orderStatusEnum;
+    }
+    
+    //Getters and Setters
+
+    public long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(long orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public int getEstablishmentId() {
+        return establishmentId;
+    }
+
+    public void setEstablishmentId(int establishmentId) {
+        this.establishmentId = establishmentId;
+    }
+
+    public int getCashId() {
+        return cashRegisterId;
+    }
+
+    public void setCashId(int cashId) {
+        this.cashRegisterId = cashId;
+    }
+
+    public int getCashierOperatorId() {
+        return cashierOperatorId;
+    }
+
+    public void setCashierOperatorId(int cashierOperatorId) {
+        this.cashierOperatorId = cashierOperatorId;
+    }
+
+    public OrderStatusEnum getOrderStatusEnum(String orderStatus) {
+        OrderStatusEnum orderStatusEnum = null;
+        if(orderStatus.equalsIgnoreCase("REGISTERED")){
+            orderStatusEnum = OrderStatusEnum.REGISTERED;
+        }
+        if(orderStatus.equalsIgnoreCase("PAID")){
+            orderStatusEnum = OrderStatusEnum.PAID;
+        }
+        if(orderStatus.equalsIgnoreCase("DELIVERED")){
+            orderStatusEnum = OrderStatusEnum.DELIVERED;
+        }
+        return orderStatusEnum;
+    }
+
+    public void setOrderStatusEnum(OrderStatusEnum orderStatusEnum) {
+        this.orderStatusEnum = orderStatusEnum;
+    }
+    
+    //To String
+    @Override
+    public String toString() {
+        return "Order{" + "orderId=" + orderId + ", customerId=" + customerId + ", establishmentId=" + establishmentId + ", cashId=" + cashRegisterId + ", cashierOperatorId=" + cashierOperatorId + ", orderStatusEnum=" + orderStatusEnum + '}';
+    }
+    
+}

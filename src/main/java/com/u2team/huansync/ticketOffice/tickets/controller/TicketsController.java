4447c8548828fee3008f3e199e97a5abe26b24bc
Ancreem <ancreemroyal@gmail.com>
2024-01-25 21:58:43 -0500

    feat: :bulb: Comments added for entire ticket section.

--- src/main/java/com/u2team/huansync/ticketOffice/tickets/controller/TicketsController.java
+++ src/main/java/com/u2team/huansync/ticketOffice/tickets/controller/TicketsController.java
@@ -5,26 +5,57 @@ import com.u2team.huansync.ticketOffice.tickets.model.classes.Tickets;
 
 import java.util.List;
 
+/**
+ * Controlador para manejar operaciones relacionadas con los tickets.
+ */
 public class TicketsController {
+
+    // Objeto TicketsDAO para realizar operaciones de acceso a datos
     private static TicketsDAO ticketsDAO = new TicketsDAO();
 
-    public static Tickets getById(long ticketsId){
+    /**
+     * Obtiene un ticket por su ID.
+     *
+     * @param ticketsId ID del ticket que se desea obtener.
+     * @return Objeto Tickets correspondiente al ID proporcionado.
+     */
+    public static Tickets getById(long ticketsId) {
         return ticketsDAO.getById(ticketsId);
     }
 
-    public static List<Tickets> getAllTickets(){
+    /**
+     * Obtiene todos los tickets.
+     *
+     * @return Lista de objetos Tickets que representan todos los tickets disponibles.
+     */
+    public static List<Tickets> getAllTickets() {
         return ticketsDAO.getAll();
     }
 
-    public static void insertTickets(Tickets tickets){
+    /**
+     * Inserta un nuevo ticket en la base de datos.
+     *
+     * @param tickets Objeto Tickets que se va a insertar.
+     */
+    public static void insertTickets(Tickets tickets) {
         ticketsDAO.save(tickets);
     }
 
-    public static void deleteTickets(long ticketsId){
+    /**
+     * Elimina un ticket por su ID.
+     *
+     * @param ticketsId ID del ticket que se desea eliminar.
+     */
+    public static void deleteTickets(long ticketsId) {
         ticketsDAO.delete(ticketsId);
     }
 
-    public static void updateTickets(Tickets tickets){
+    /**
+     * Actualiza la información de un ticket existente.
+     *
+     * @param tickets Objeto Tickets con la información actualizada.
+     */
+    public static void updateTickets(Tickets tickets) {
         ticketsDAO.update(tickets);
     }
 }
